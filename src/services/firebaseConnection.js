04d9336d0ba5b262fd063824213968965c6239b7
Breno098 <brenohenrique098@gmail.com>
2020-11-30 22:48:08 -0300

    Initial commit

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+import 'firebase/database';
+import 'firebase/auth';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyApOxdU2S3rWDMa9bexKqT-CczF3qv1BXA",
+    authDomain: "financesapp-bef8a.firebaseapp.com",
+    databaseURL: "https://financesapp-bef8a.firebaseio.com",
+    projectId: "financesapp-bef8a",
+    storageBucket: "financesapp-bef8a.appspot.com",
+    messagingSenderId: "1029341625428",
+    appId: "1:1029341625428:web:327b8887dbe382dad001c1",
+    measurementId: "G-ZVTB369TEQ"
+  };
+
+if(firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
